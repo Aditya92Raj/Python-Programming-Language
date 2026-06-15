cc676b29daecaa656b403e48373c61ef2392f2d9
Aditya Raj <adityarajadityaraj893@gmail.com>
2026-06-15 18:27:59 +0530

    7  Python Basic program.py

---
+++ 1 Python Basics/7  Python Basic program.py
@@ -0,0 +1,13 @@
+#Basic program in python.
+a=50
+b=25
+Sum=a+b
+difference=a-b
+multiply=a*b
+divide=a/b
+print("a=",a)
+print("b=",b)
+print("Sum=",Sum)
+print("difference=",difference)
+print("multiply=",multiply)
+print("divide=",divide)
