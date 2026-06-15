25c1d88d9a136c2b6a42c2325338a52d316f89c6
Aditya Raj <adityarajadityaraj893@gmail.com>
2026-06-15 18:38:29 +0530

    1 Single line comments.py

---
+++ 1 Python Basics/8 Comments in python/1 Single line comments.py
@@ -0,0 +1,14 @@
+#Comments in python:-
+#Single line comments with the help of hush(#).
+a=68
+b=40
+Sum=a+b
+difference=a-b
+multiply=a*b
+divide=a/b
+print("a=",a)
+print("b=",b)
+print("Sum=",Sum)
+print("difference=",difference)
+#print("multiply=",multiply)
+#print("divide=",divide)
