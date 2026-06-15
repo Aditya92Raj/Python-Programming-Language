a73e9397bc2ac09e20777545797eaa284b897e75
Aditya Raj <adityarajadityaraj893@gmail.com>
2026-06-15 15:47:11 +0530

    Data type.py

---
+++ Basics/Data type.py
@@ -0,0 +1,10 @@
+#Everything is called data.
+#There are Many types of Data:-
+Name="Aditya Raj"
+Age=18
+Marks=92.4
+Statement=True
+print(Name,type(Name))
+print(Age,type(Age))
+print(Marks,type(Marks))
+print(Statement,type(Statement))
