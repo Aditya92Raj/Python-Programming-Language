56f18c2eb83a63d9070cdf0a8ee126876dc4dd9e
Aditya Raj <adityarajadityaraj893@gmail.com>
2026-06-15 15:15:03 +0530

    Create Variable & Data type.py

---
+++ Basic/Variable & Data type.py
@@ -0,0 +1,11 @@
+#Data Type in Python:-
+#Everything is called data.
+Name="Aditya Raj"
+Age=18
+Marks=92.4
+Statement=True
+print(Name,type(Name))
+print(Age,type(Age))
+print(Marks,type(Marks))
+print(Statement,type(Statement))
+#Variable is a container that stores various type of data in program.
