f2bb34e101eaf4bb2ea8211622cdd5de7996e5ab
Aditya Raj <adityarajadityaraj893@gmail.com>
2026-06-16 08:58:21 +0530

    1 Input_Output.py

---
+++ 1 Python Basics/10 Input & Output/1 Input_Output.py
@@ -0,0 +1,8 @@
+#Input in python
+print("Give me your information")
+input("enter your name:")
+input("enter your age:")
+input("enter your college:")
+input("enter your course:")
+print("Thankyou...")
+#input statement is used to accept data(using keyboard) from user.
