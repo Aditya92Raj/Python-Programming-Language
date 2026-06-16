8aebe3a5ba34147cc742170bf26a73fc4c3b0ac1
Aditya Raj <adityarajadityaraj893@gmail.com>
2026-06-16 15:42:23 +0530

    Create 02 Basic_Input_Output_program.py

---
+++ 01 Python Basics/10 Input & Output/02 Basic_Input_Output_program.py
@@ -0,0 +1,9 @@
+#Input in python
+a=int(input("Enter your first number:"))
+b=int(input("Enter your second number:"))
+print("a=",a)
+print("b=",b)
+print("sum=",a+b)
+print("sum=",a-b)
+print("multiply=",a*b)
+print("divide=",a/b)
