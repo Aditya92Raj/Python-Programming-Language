ba6c747e45c9cbb3ba0d6b526b77dda05c7609da
Aditya Raj <adityarajadityaraj893@gmail.com>
2026-06-16 08:30:21 +0530

    1 Implicit.py

---
+++ 1 Python Basics/9 Type-Casting/1 Implicit.py
@@ -0,0 +1,12 @@
+#Type casting in python:-
+#Implicit type casting
+a=15
+b=22.5
+Sum=a+b
+print("a=",a)
+print(type(a))
+print("b=",b)
+print(type(b))
+print("Sum=",Sum)
+print(type(Sum))
+#In Implicit type casting the data type change automatically according to operation.
