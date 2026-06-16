fc3a7c643b1c9d4f5927c0ea43eacfdf0bb380e7
Aditya Raj <adityarajadityaraj893@gmail.com>
2026-06-16 15:57:51 +0530

    Create 04 swapping with third variable.py

---
+++ 01 Python Basics/10 Input & Output/04 swapping with third variable.py
@@ -0,0 +1,13 @@
+#Input in python
+#Swapping numbers with the help of third variable. 
+a=int(input("Enter your first number:"))
+b=int(input("Enter your second number:"))
+print("Before swapping")
+print("a=",a)
+print("b=",b)
+c=a
+a=b
+b=c
+print("After swapping")
+print("a=",a)
+print("b=",b)
