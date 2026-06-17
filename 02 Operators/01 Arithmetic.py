65c1c6bd4db9e672910e672e9b1429665868c223
Aditya Raj <adityarajadityaraj893@gmail.com>
2026-06-17 08:07:03 +0530

    Create 01 Arithmetic.py

---
+++ 02 Operators/01 Arithmetic.py
@@ -0,0 +1,18 @@
+#Operator in python:-
+#Arithmetic Operator(+,-,*,/,%,**)
+a=9
+b=3
+Sum=a+b
+difference=a-b
+multiply=a*b
+divide=a/b
+remainder=a%b
+power=a**b
+print("a=",a)
+print("b=",b)
+print("Sum=",Sum)
+print("difference=",difference)
+print("multiply=",multiply)
+print("divide=",divide)
+print("remainder=",remainder)
+print("power=",power)
