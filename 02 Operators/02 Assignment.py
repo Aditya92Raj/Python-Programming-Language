8425dccbb6ac0c6813cdfedab53db2046e38686e
Aditya Raj <adityarajadityaraj893@gmail.com>
2026-06-18 13:14:39 +0530

    Create 02 Assignment.py

---
+++ 02 Operators/02 Assignment.py
@@ -0,0 +1,38 @@
+#Operator in python:-
+#Assignment Operator(+=,-=,*=,/=,%=,**=)
+a=12
+print("Before value Assign")
+print("a=",a)
+a+=18
+print("After value Assign")
+print("a=",a)
+b=24
+print("Before value Assign")
+print("b=",b)
+b-=14
+print("After value Assign")
+print("b=",b)
+c=8
+print("Before value Assign")
+print("c=",c)
+c*=5
+print("After value Assign")
+print("c=",c)
+d=60
+print("Before value Assign")
+print("d=",d)
+d/=20
+print("After value Assign")
+print("d=",d)
+p=41
+print("Before value Assign")
+print("p=",p)
+p%=7
+print("After value Assign")
+print("p=",p)
+q=2
+print("Before value Assign")
+print("q=",q)
+q**=5
+print("After value Assign")
+print("q=",q)
