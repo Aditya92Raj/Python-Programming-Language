d387364e4e5fe9c0a941303df402b1fdd626bf0d
Aditya Raj <adityarajadityaraj893@gmail.com>
2026-06-19 09:27:27 +0530

    Create 02 Even_Odd program.py

---
+++ 03 Conditional Statement/02 Even_Odd program.py
@@ -0,0 +1,6 @@
+#Write a program to input number from user and check wether given number is Even or Odd.
+a=int(input("enter a number:"))
+if a%2==0:
+    print("Even number:")
+else:
+    print("Odd number")
