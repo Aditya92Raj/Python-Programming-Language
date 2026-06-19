5d287a72a79c029d4bdb1a3d98ef2b1dce7ea8bf
Aditya Raj <adityarajadityaraj893@gmail.com>
2026-06-19 08:40:43 +0530

    Create 04 Logical.py

---
+++ 02 Operators/04 Logical.py
@@ -0,0 +1,12 @@
+#Operator in python:-
+#Logical operator(and, or, not)
+a=50
+b=30
+print("a=",a)
+print("b=",b)
+print("(a==b and a>=b)",a==b and a>=b)
+print("(a!=b and a<b)",a!=b and a<b)
+print("(a>=b or a==b)",a>=b or a==b)
+print("(not a==b)",not a==b)
+print("(not a<=b)",not a<=b)
+#It always gives output at "True" or "False"
