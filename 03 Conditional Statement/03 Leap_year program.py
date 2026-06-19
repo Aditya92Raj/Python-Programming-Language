8e567645d74150d5932f357085b77cd235ff6f85
Aditya Raj <adityarajadityaraj893@gmail.com>
2026-06-19 09:37:34 +0530

    Create 03 Leap_year program.py

---
+++ 03 Conditional Statement/03 Leap_year program.py
@@ -0,0 +1,6 @@
+#Write a program to input year from user and check wether given year is leap year or not.
+a=int(input("enter a year:"))
+if a%4==0:
+    print("Leap year:")
+else:
+    print("Not leap year")
