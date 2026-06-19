4f42b926a31c6e70fa253f0833ae8e675d077200
Aditya Raj <adityarajadityaraj893@gmail.com>
2026-06-19 08:26:59 +0530

    Create 03 Conditional or Relational.py

---
+++ 02 Operators/03 Conditional or Relational.py
@@ -0,0 +1,13 @@
+#Operator in python:-
+#Conditional or Relational or Comparisonal operator(==,!=,<=,>=,<,>)
+a=50
+b=20
+print("a=",a)
+print("b=",b)
+print("a==b",a==b)
+print("a!=b",a!=b)
+print("a<b",a<b)
+print("a>b",a>b)
+print("a<=b",a<=b)
+print("a>=b",a>=b)
+#It always gives output at "True" or "False"
