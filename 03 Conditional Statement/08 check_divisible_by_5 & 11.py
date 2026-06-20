0f2fe6ed282337d3039b11dcbc2c741add1d7ce5
Aditya Raj <adityarajadityaraj893@gmail.com>
2026-06-20 11:52:07 +0530

    Update 08 check_divisible_by_5 & 11.py

--- 03 Conditional Statement/08 check_divisible_by_5 & 11.py
+++ 03 Conditional Statement/08 check_divisible_by_5 & 11.py
@@ -1,5 +1,5 @@
 #Write a program to check whether a number is divisible by both 5 and 11.
-a=int(input("Enter a character:"))
+a=int(input("Enter a number:"))
 if a%5==0 and a%11==0:
     print("Divisible by 5 and 11")
 else:
