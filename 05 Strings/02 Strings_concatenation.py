4ddc85389ae3138bba1ed46366f57c9c0a73d9ed
Aditya Raj <adityarajadityaraj893@gmail.com>
2026-06-21 15:04:23 +0530

    Create 02 Strings_concatenation.py

---
+++ 05 Strings/02 Strings_concatenation.py
@@ -0,0 +1,11 @@
+#String in Python:-
+#Concatenation (Basic operation)
+a="Butter"
+print("a:",a)
+print(type(a))
+b="fly"
+print("b:",b)
+print(type(b))
+z=a+b
+print("concatenate:",z)
+print(type(z))
