285c954be1e817a6514433bf40d8aa42aee3e49d
Aditya Raj <adityarajadityaraj893@gmail.com>
2026-06-21 15:19:33 +0530

    Create 04 String_Indexing.py

---
+++ 05 Strings/04 String_Indexing.py
@@ -0,0 +1,11 @@
+#String in Python:-
+#Indexing (position)
+a="Aditya Raj"
+print("a:",a)
+print(type(a))
+print(a[1])
+print(type(a[1]))
+print(a[3])
+print(type(a[3]))
+#Basically we use indexing to define the position of "letter", "word" and etc in string.
+#Indexing always stars from 0.
