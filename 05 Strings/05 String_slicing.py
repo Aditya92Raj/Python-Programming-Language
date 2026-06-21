c8cc52bc7569aeec0da18d5ca1bf42f37d6b2d21
Aditya Raj <adityarajadityaraj893@gmail.com>
2026-06-21 15:27:32 +0530

    Create 05 String_slicing.py

---
+++ 05 Strings/05 String_slicing.py
@@ -0,0 +1,14 @@
+#String in Python:-
+#String slicing (substring)
+a="HINDUSTAN"
+print("a:",a)
+print(type(a))
+print(a[1:5])
+print(type(a[1:5]))
+print(a[0:9])
+print(type(a[0:9]))
+print(a[5:])
+print(type(a[5:]))
+print(a[:len(a)])
+print(type(a[:len(a)]))
+#slicing of string or substring is a part of string.
