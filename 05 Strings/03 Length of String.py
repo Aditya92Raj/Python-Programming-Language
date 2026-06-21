2a3d6201dd826d65fd6d9db0e0a31e562bf35dc5
Aditya Raj <adityarajadityaraj893@gmail.com>
2026-06-21 15:09:51 +0530

    Create 03 Length of String.py

---
+++ 05 Strings/03 Length of String.py
@@ -0,0 +1,7 @@
+#String in Python:-
+#Length of string (Basic operation)
+a="Aditya Raj"
+print("a:",a)
+print(type(a))
+print("length of string:",len(a))
+#space is always included in string length.
