4cf9b00ffce3dd6d92d39a3badaf8dccc0114d61
Aditya Raj <adityarajadityaraj893@gmail.com>
2026-06-22 09:08:28 +0530

    Create 01 str_endswith.py

---
+++ 05 Strings/06 Strings Method/01 str_endswith.py
@@ -0,0 +1,9 @@
+#String Method:-
+#string.endswith(element)
+a="Oxford Business College"
+print("a=",a)
+print(type(a))
+print(a.endswith("e"))
+print(a.endswith("lege"))
+print(a.endswith("College"))
+#This Method is used to compare the last element same or not.
