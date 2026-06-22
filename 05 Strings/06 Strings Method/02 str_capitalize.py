89458cfd878678fa5f3aeabe2c293f43fb974a23
Aditya Raj <adityarajadityaraj893@gmail.com>
2026-06-22 11:04:27 +0530

    Create 02 str_capitalize.py

---
+++ 05 Strings/06 Strings Method/02 str_capitalize.py
@@ -0,0 +1,7 @@
+#Strings Method:-
+#string.capitalize()
+name="adItYa rAJ"
+print("name=",name)
+print(type(name))
+print(name.capitalize())
+#This method is used to change the first letter Capital and all remain letter small.
