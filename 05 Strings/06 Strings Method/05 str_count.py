a995eeadb9d7c9732303d7de40310cac89c5b391
Aditya Raj <adityarajadityaraj893@gmail.com>
2026-06-22 11:46:36 +0530

    Create 05 str_count.py

---
+++ 05 Strings/06 Strings Method/05 str_count.py
@@ -0,0 +1,5 @@
+#Strings Method:-
+#string.count(element)
+a="Oxford College of Research and Management"
+print(a.count("e"))
+#This method is used to count the all character, word or sentence in given strings.
