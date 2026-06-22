bbdfa6d6cce228097dddbaf0d576bec11bafb9e2
Aditya Raj <adityarajadityaraj893@gmail.com>
2026-06-22 11:41:46 +0530

    Create 04 str_find.py

---
+++ 05 Strings/06 Strings Method/04 str_find.py
@@ -0,0 +1,5 @@
+#Strings Method:-
+#string.find(element)
+a="Oxford College of Research and Management"
+print(a.find("d"))
+#This method is used to find the character, word or sentence of 1st occurrence in strings.
