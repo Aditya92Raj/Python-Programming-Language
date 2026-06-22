eb7cec7b3c44d6ddb757b6989f3093ec3796f1a4
Aditya Raj <adityarajadityaraj893@gmail.com>
2026-06-22 11:25:25 +0530

    Create 03 str_replace.py

---
+++ 05 Strings/06 Strings Method/03 str_replace.py
@@ -0,0 +1,11 @@
+#Strings Method:-
+#string.replace(old,new)
+name="Sita"
+print("name=",name)
+print(type(name))
+print("change name=",name.replace('S','G'))
+college="Oxford business college"
+print("college=",college)
+print(type(college))
+print("change college name=",college.replace('business','law'))
+#This method is used to replace the character, word or sentence in strings.
