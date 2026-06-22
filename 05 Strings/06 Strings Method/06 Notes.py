cf0ad0d9d729a755f9b36afad5fbcc6c1b808119
Aditya Raj <adityarajadityaraj893@gmail.com>
2026-06-22 11:58:41 +0530

    Create 06 Notes.py

---
+++ 05 Strings/06 Strings Method/06 Notes.py
@@ -0,0 +1,5 @@
+#Note:-(i)String is not mutable(change) and allow duplicates.
+#(ii) List is mutable(change) and allow duplicates.
+#(iii) Tuple is immutable(not mutable) and allow duplicates.
+#(iv) Dictionary is mutable and not allow duplicates.
+#(v) Set is immutable and not allow duplicates.
