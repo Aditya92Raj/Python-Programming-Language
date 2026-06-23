942c1347228ffc38d5a675e484d418aaa9ebea1c
Aditya Raj <adityarajadityaraj893@gmail.com>
2026-06-23 09:41:25 +0530

    Update 03 list_sort(reverse=True).py

--- 06 Lists/04 Lists Method/03 list_sort(reverse=True).py
+++ 06 Lists/04 Lists Method/03 list_sort(reverse=True).py
@@ -4,4 +4,4 @@ a=[12,19,8,25]
 print("list=",a)
 a.sort(reverse=True)
 print("new list=",a)
-#Basically we use sort method in list for create assending order same type of data (element) in list.
+#Basically we use sort(reverse=True) method in list for create decending order same type of data (element) in list.
