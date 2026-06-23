df974c0f6205b58810d104c6c88e67fdf1b847ad
Aditya Raj <adityarajadityaraj893@gmail.com>
2026-06-23 09:34:20 +0530

    Create 04 list_reverse.py

---
+++ 06 Lists/04 Lists Method/04 list_reverse.py
@@ -0,0 +1,7 @@
+#List Method:-
+#list.reverse()
+a=[12,19,8,25]
+print("list=",a)
+a.reverse()
+print("new list=",a)
+#Basically we use sort method in list for reverse the order same type of data (element) in list.
