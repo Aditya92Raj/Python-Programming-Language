d2fbe89396141f05bb0ea6f1893989a652042b3a
Aditya Raj <adityarajadityaraj893@gmail.com>
2026-06-23 09:19:16 +0530

    Create 01 list_append.py

---
+++ 06 Lists/04 Lists Method/01 list_append.py
@@ -0,0 +1,7 @@
+#List Method:-
+#list.append(element)
+a=["Aditya Raj",20]
+print("list=",a)
+a.append(82.6)
+print("new list=",a)
+#Basically we use append method in list for add any type of data (element) in list at the last index.
