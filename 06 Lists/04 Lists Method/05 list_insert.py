d387def6ad32b21ceea06b44391e01d9b1d1ebff
Aditya Raj <adityarajadityaraj893@gmail.com>
2026-06-23 09:40:03 +0530

    Create 05 list_insert.py

---
+++ 06 Lists/04 Lists Method/05 list_insert.py
@@ -0,0 +1,7 @@
+#List Method:-
+#list.insert(index,element)
+a=["Aditya Raj",82.6,True]
+print("list=",a)
+a.insert(1,20)
+print("new list=",a)
+#Basically we use insert method in list for add the type of data (element) in list at the specific index.
