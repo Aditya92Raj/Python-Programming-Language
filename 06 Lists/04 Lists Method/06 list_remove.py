0286efd1061a57c02ca567b0581569d514241ec3
Aditya Raj <adityarajadityaraj893@gmail.com>
2026-06-23 09:51:47 +0530

    Create 06 list_remove.py

---
+++ 06 Lists/04 Lists Method/06 list_remove.py
@@ -0,0 +1,7 @@
+#List Method:-
+#list.remove(element)
+a=["Aditya Raj",20,82.6,True,20]
+print("list=",a)
+a.remove(20)
+print("new list=",a)
+#Basically we use remove method in list for remove the any type of data (element) in list at first occurrence.
