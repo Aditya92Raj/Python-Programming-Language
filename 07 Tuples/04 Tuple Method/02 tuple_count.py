e0a705cc7592f0ebdcf1af8006da5ff988a26af4
Aditya Raj <adityarajadityaraj893@gmail.com>
2026-06-24 20:00:15 +0530

    Create 02 tuple_count.py

---
+++ 07 Tuples/04 Tuple Method/02 tuple_count.py
@@ -0,0 +1,7 @@
+#Tuple in python:-
+#tuple.count(element)
+a=(2,1,8,6,5,1,4)
+print("tuple=",a)
+print(type(a))
+print("Total element of 6=",a.count(6))
+#This method is used to count the total elements of the same element in tuple.
