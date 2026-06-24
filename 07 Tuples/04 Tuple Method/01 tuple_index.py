29ead84bebd89c52564d30fab4366cac8b4d11fa
Aditya Raj <adityarajadityaraj893@gmail.com>
2026-06-24 19:50:56 +0530

    Create 01 tuple_index.py

---
+++ 07 Tuples/04 Tuple Method/01 tuple_index.py
@@ -0,0 +1,7 @@
+#Tuple in python:-
+#tuple.index(element)
+a=(2,1,8,6,5,1,4)
+print("tuple=",a)
+print(type(a))
+print("index of 6=",a.index(6))
+#This method is used to find the index of element in tuple.
