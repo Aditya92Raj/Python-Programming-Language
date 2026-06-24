07e5ef2dc900c3d7df0841c1414140674f5aa605
Aditya Raj <adityarajadityaraj893@gmail.com>
2026-06-24 09:35:49 +0530

    Create 07 list_pop.py

---
+++ 06 Lists/04 Lists Method/07 list_pop.py
@@ -0,0 +1,7 @@
+#List Method:-
+#list.pop(index)
+a=["Aditya Raj",20,82.6,20,True,20]
+print("list=",a)
+a.pop(3)
+print("new list=",a)
+#Basically we use remove method in list for remove the any type of data (element) in list at specific index.
