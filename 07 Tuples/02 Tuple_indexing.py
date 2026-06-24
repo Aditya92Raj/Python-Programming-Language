fc52b87433fc6447a643835853917ac8c8a23199
Aditya Raj <adityarajadityaraj893@gmail.com>
2026-06-24 19:28:50 +0530

    Create 02 Tuple_indexing.py

---
+++ 07 Tuples/02 Tuple_indexing.py
@@ -0,0 +1,15 @@
+#Tuple in Python:-
+#Indexing (position)
+a=("Aditya Raj",20,82.6,True)
+print("Tuple=",a)
+print(type(a))
+print("Name=",a[0])
+print(type(a[0]))
+print("Age=",a[1])
+print(type(a[1]))
+print("Marks=",a[2])
+print(type(a[2]))
+print("Statement=",a[3])
+print(type(a[3]))
+#Basically we use indexing to define the position of multiple types of data in Tuple.
+#Indexing always stars from 0.
