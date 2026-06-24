f525b50036d946010836d3b27108b857495d736a
Aditya Raj <adityarajadityaraj893@gmail.com>
2026-06-24 19:53:40 +0530

    Update 01 tuple_index.py

--- 07 Tuples/04 Tuple Method/01 tuple_index.py
+++ 07 Tuples/04 Tuple Method/01 tuple_index.py
@@ -4,4 +4,4 @@ a=(2,1,8,6,5,1,4)
 print("tuple=",a)
 print(type(a))
 print("index of 6=",a.index(6))
-#This method is used to find the index of element in tuple.
+#This method is used to find the index of 1st occurrence of element in tuple.
