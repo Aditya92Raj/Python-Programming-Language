fbc2ec03e443f5be7a74ffec1d41b42a75925137
Aditya Raj <adityarajadityaraj893@gmail.com>
2026-06-25 14:29:14 +0530

    Create 01 Nested_Dictionary_program.py

---
+++ 08 Dictionaries/02 Nested Dictionary/01 Nested_Dictionary_program.py
@@ -0,0 +1,5 @@
+# Nested Dictionary in python:-
+a={"Name":"Aditya Raj","Subject":{"Physics":84,"Chemistry":78,"Maths":92}}
+print("Student=",a)
+print(type(a))
+# Nested Dictionary is used to store different types of data in single memory at pair (key:value) form.
