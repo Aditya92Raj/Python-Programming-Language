ae0fda9165b373eb6ff8225b695b6545ecdfc773
Aditya Raj <adityarajadityaraj893@gmail.com>
2026-06-25 14:13:29 +0530

    Create 01 Dictionary_data_type.py

---
+++ 08 Dictionaries/01 Dictionary_data_type.py
@@ -0,0 +1,5 @@
+#Dictionary in python:-
+a={"Name":"Aditya Raj","Age":"20","Course":"BCA"}
+print("Dictionary=",a)
+print(type(a))
+#Dictionary is used to store different types of data in single memory at pair (key:value) form.
