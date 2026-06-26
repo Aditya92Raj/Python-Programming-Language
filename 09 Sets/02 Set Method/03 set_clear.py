25597c815aeeaf78ab83d0a8439ab0ed02ff65ba
Aditya Raj <adityarajadityaraj893@gmail.com>
2026-06-26 18:16:50 +0530

    Create 03 set_clear.py

---
+++ 09 Sets/02 Set Method/03 set_clear.py
@@ -0,0 +1,10 @@
+#Set in python:-
+#set.remove(element)
+a={1,3,9,7,8,2}
+print("Set=",a)
+print(type(a))
+a.clear()
+print("new set=",a)
+print("length of set=",len(a))
+#This method is used to remove all the element at a time from set.
+#len(set) is 0 that means no any element in this set.
