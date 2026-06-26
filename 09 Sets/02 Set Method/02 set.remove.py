97da0b6436c64b9308a7fe1926ad12173c220722
Aditya Raj <adityarajadityaraj893@gmail.com>
2026-06-26 18:07:09 +0530

    Create 02 set.remove.py

---
+++ 09 Sets/02 Set Method/02 set.remove.py
@@ -0,0 +1,8 @@
+#Set in python:-
+#set.remove(element)
+a={1,3,9,7,8,2}
+print("Set=",a)
+print(type(a))
+a.remove(9)
+print("new set=",a)
+#This method is used to remove the one element at a time from set.
