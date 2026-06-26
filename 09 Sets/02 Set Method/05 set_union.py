413867072bf226d6e86c7a09edc0f7535693779b
Aditya Raj <adityarajadityaraj893@gmail.com>
2026-06-26 18:31:53 +0530

    Create 05 set_union.py

---
+++ 09 Sets/02 Set Method/05 set_union.py
@@ -0,0 +1,11 @@
+#Set in python:-
+#set1.union(set2)
+a={5,7,3,8}
+print("Set1=",a)
+print(type(a))
+b={2,6,4,1,9}
+print("Set2=",b)
+print(type(b))
+c=a.union(b)
+print("new combine set=",c)
+#This method is used to combine two sets.
