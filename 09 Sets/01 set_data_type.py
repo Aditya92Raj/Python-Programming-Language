934a42fb5a1cbd0dfd123d5b7d1e826720f41243
Aditya Raj <adityarajadityaraj893@gmail.com>
2026-06-26 17:44:32 +0530

    Create 01 set_data_type.py

---
+++ 09 Sets/01 set_data_type.py
@@ -0,0 +1,6 @@
+#Set in python:-
+a={1,3,9,6,4,7,8,2}
+print("Set=",a)
+print(type(a))
+#Set is used to store different types of data in single memory and it is collection of unordered items.
+#Each element in the set must be unique and immutable.
