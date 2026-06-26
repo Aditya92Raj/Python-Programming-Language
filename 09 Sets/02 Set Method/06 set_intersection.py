9af804c8298af046e3f18c1e79d69bb4fc84d902
Aditya Raj <adityarajadityaraj893@gmail.com>
2026-06-26 18:35:30 +0530

    Create 06 set_intersection.py

---
+++ 09 Sets/02 Set Method/06 set_intersection.py
@@ -0,0 +1,11 @@
+#Set in python:-
+#set1.intersection(set2)
+a={5,7,3,8,6}
+print("Set1=",a)
+print(type(a))
+b={2,7,1,5,9,3}
+print("Set2=",b)
+print(type(b))
+c=a.intersection(b)
+print("new combine set=",c)
+#This method is used to combine the common value two sets and create new set.
