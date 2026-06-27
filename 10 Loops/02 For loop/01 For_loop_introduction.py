2d5c297524b470400dcf93ad4aca304c8d9b60ac
Aditya Raj <adityarajadityaraj893@gmail.com>
2026-06-27 12:26:20 +0530

    Create 01 For_loop_introduction.py

---
+++ 10 Loops/02 For loop/01 For_loop_introduction.py
@@ -0,0 +1,15 @@
+# For loop in python:-
+#A for loop is used to itrate (go through) a sequence (like-list,range,tuple,dictionary,set)
+
+#syntax of For loop:-
+#for variable in sequence:
+#    statement...
+
+#range functions returns a sequence of number from 0 (by default) and increments by 1 (by default) and stops before a specified number.
+
+#range(starts,stops,steps)
+a=[2,6,4,5,8]
+print("List=",a)
+print(type(a))
+for x in a:
+  print(x)
