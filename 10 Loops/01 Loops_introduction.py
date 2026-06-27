01ee375618c441fdaa7f95f2ef8601a639851408
Aditya Raj <adityarajadityaraj893@gmail.com>
2026-06-27 12:37:15 +0530

    Update 01 Loops_introduction.py

--- 10 Loops/01 Loops_introduction.py
+++ 10 Loops/01 Loops_introduction.py
@@ -3,3 +3,13 @@
 #There are two types of loop:-
 #(i)for loop
 #(ii)while loop
+a=[4,6,3,5,7]
+print("List=",a)
+print(type(a))
+for i in a:
+  print(i)
+b=1
+n=int(input("Enter a number for table:"))
+while b<=10:
+    print(n*b)
+    b+=1
