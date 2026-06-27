b46d7287b8d9b55104e47f0826527f7f6228cbf3
Aditya Raj <adityarajadityaraj893@gmail.com>
2026-06-27 13:57:06 +0530

    Create 05 Loop_continue.py

---
+++ 10 Loops/05 Loop_continue.py
@@ -0,0 +1,5 @@
+#The continue statement is used to skip the current iteration of a loop (for or while) and continue with the next iteration without terminating the loop.
+for i in range(1,6):
+    if i==3:
+        continue
+    print(i)
