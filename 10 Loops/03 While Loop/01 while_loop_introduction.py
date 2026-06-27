9f99787c2d88852bff4297c26434785334e6ade7
Aditya Raj <adityarajadityaraj893@gmail.com>
2026-06-27 13:17:51 +0530

    Create 01 while_loop_introduction.py

---
+++ 10 Loops/03 While Loop/01 while_loop_introduction.py
@@ -0,0 +1,9 @@
+#While loop in python:-
+#A while is used to repeatedly execute a block of code as long as a given condition is True.
+#while loop syntax:-
+#while (condition):
+#    some work...
+a=2
+while a<=8:
+    print(a)
+    a+=1
