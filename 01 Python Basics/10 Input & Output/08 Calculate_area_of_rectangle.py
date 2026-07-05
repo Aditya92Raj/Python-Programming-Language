5a117a4c4340fb3a8ffc47bff0aa90286323c874
Aditya Raj <adityarajadityaraj893@gmail.com>
2026-07-05 14:01:38 +0530

    Create 08 Calculate_area_of_rectangle.py

---
+++ 01 Python Basics/10 Input & Output/08 Calculate_area_of_rectangle.py
@@ -0,0 +1,4 @@
+#Write a program to calculate area of square according to given length and breadth.
+l=int(input("Enter length of square:"))
+b=int(input("Enter breadth of square:"))
+print("Area of square=",l*b)
