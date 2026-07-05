da90d30495365d52aa92fe212f46b370cb47660a
Aditya Raj <adityarajadityaraj893@gmail.com>
2026-07-05 14:26:10 +0530

    Create 11 Calculate_Perimeter_of_rectangle.py

---
+++ 01 Python Basics/10 Input & Output/11 Calculate_Perimeter_of_rectangle.py
@@ -0,0 +1,4 @@
+#Write a program to calculate perimeter of rectangle according to given lenth and breadth.
+l=int(input("Enter length of rectangle:"))
+b=int(input("Enter breath of rectangle:"))
+print("Perimeter of rectangle=",2*(l+b))
