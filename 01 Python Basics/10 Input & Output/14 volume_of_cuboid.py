47de33cfdff1e724985caac6c2ffb23728a23d28
Aditya Raj <adityarajadityaraj893@gmail.com>
2026-07-06 14:28:46 +0530

    Create 14 volume_of_cuboid.py

---
+++ 01 Python Basics/10 Input & Output/14 volume_of_cuboid.py
@@ -0,0 +1,5 @@
+#Write a program to calculate volume of cuboid according to given length, breadth and height.
+l=int(input("Enter length of cuboid:"))
+b=int(input("Enter breadth of cuboid:"))
+h=int(input("Enter height of cuboid:"))
+print("volume of cuboid=",l*b*h)
