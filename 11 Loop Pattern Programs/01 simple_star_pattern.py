a21b91be0d1449f7188213bd0b4cc98928107159
Aditya Raj <adityarajadityaraj893@gmail.com>
2026-07-06 13:04:56 +0530

    Create 01 simple_star_pattern.py

---
+++ 11 Loop Pattern Programs/01 simple_star_pattern.py
@@ -0,0 +1,4 @@
+#Write a program to input number from user and print star pattern according to given number with the help of loop.
+a=int(input("Enter a number:"))
+for x in range(a):
+    print("*"*a)
