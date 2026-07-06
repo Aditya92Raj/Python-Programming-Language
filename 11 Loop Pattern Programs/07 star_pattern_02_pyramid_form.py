99453814d4353c76b109a9a347a3805ae41edf11
Aditya Raj <adityarajadityaraj893@gmail.com>
2026-07-06 14:20:03 +0530

    Create 07 star_pattern_02_pyramid_form.py

---
+++ 11 Loop Pattern Programs/07 star_pattern_02_pyramid_form.py
@@ -0,0 +1,4 @@
+#Write a program to input number from user and print star pattern form of pyramid according to given number.
+a=int(input("Enter a number:"))
+for x in range(1,a+1):
+    print(" "*(x-1)+"* "*(a+1-x))
