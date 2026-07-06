3a3d858fe3c0a94313ce8f1490f23f501cc7c062
Aditya Raj <adityarajadityaraj893@gmail.com>
2026-07-06 15:03:06 +0530

    Create 15 convert_°c_to_Fahrenheit.py

---
+++ 01 Python Basics/10 Input & Output/15 convert_°c_to_Fahrenheit.py
@@ -0,0 +1,3 @@
+#Write a program to convert temparature °c to fahrenheit according to given temparature.
+C=int(input("Enter temparature in °c number form:"))
+print("Temparature in fahrenheit=",9/5*C+32)
