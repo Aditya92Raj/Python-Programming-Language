b2e4b82608d0e187ecc9564328cabb1671d7b840
Aditya Raj <adityarajadityaraj893@gmail.com>
2026-07-06 13:57:59 +0530

    Create 04 star_pattern_03_RAT_form.py

---
+++ 11 Loop Pattern Programs/04 star_pattern_03_RAT_form.py
@@ -0,0 +1,4 @@
+#Write a program to input number from user and print star pattern form of Right angle triangle according to given number.
+a=int(input("Enter a number:"))
+for x in range(1,a+1):
+    print("*"*(a+1-x))
