708c4772652a827b0fc37bc6a15e0514997b47c2
Aditya Raj <adityarajadityaraj893@gmail.com>
2026-07-07 15:57:09 +0530

    Create 14 Z_form_star_pattern.py

---
+++ 11 Loop Pattern Programs/14 Z_form_star_pattern.py
@@ -0,0 +1,9 @@
+#Write a program to input number from user and print Z form star pattern.
+a=int(input("Enter a number:"))
+for x in range(1,a+1):
+    for y in range(1,a+1):
+        if ( x==1 or x==a or y==a+1-x):
+            print("*",end="")
+        else:
+            print(" ",end="")
+    print()
