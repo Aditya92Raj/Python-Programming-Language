6d8611015c8586b530d08d5cc113a9a69ce174e6
Aditya Raj <adityarajadityaraj893@gmail.com>
2026-07-09 22:54:39 +0530

    Update 18 product_of_numbers_from_1 to n.py

--- 10 Loops/03 While Loop/18 product_of_numbers_from_1 to n.py
+++ 10 Loops/03 While Loop/18 product_of_numbers_from_1 to n.py
@@ -1,4 +1,4 @@
-#Write a program to input number from user and find the sum of numbers from 1 to n
+#Write a program to input number from user and find the product of numbers from 1 to n
 a=1
 s=1
 n=int(input("Enter a number:"))
