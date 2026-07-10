1bd52e7eede2ca6d9afb4def9fe4392fe67d5ec6
Aditya Raj <adityarajadityaraj893@gmail.com>
2026-07-10 21:07:34 +0530

    Create 01 basic_function_program.py

---
+++ 12 Functions/01 basic_function_program.py
@@ -0,0 +1,16 @@
+# Function in Python:-
+# A function is a block of code that performs a specific task.
+# In Python, we create a function using def (definition), and we run it by calling it.
+
+# Function Definition
+def greet():
+    print("Hello, Welcome to Python!")
+
+# Function Call
+greet()
+
+# def keyword is used to define a function.
+# greet is the name of the function.
+# () are parentheses (can contain parameters).
+# : indicates the start of the function body.
+# greet() is the function call.
