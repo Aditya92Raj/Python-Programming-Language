71e11dc67adf987253218b8651069a910ababc63
Aditya Raj <adityarajadityaraj893@gmail.com>
2026-07-10 21:19:18 +0530

    Create 02 function_example_with_parameters.py

---
+++ 12 Functions/02 function_example_with_parameters.py
@@ -0,0 +1,10 @@
+#Function in Python:-
+
+# Function Definition
+def student(name, age):
+    print("Name :", name)
+    print("Age  :", age)
+
+# Function Call
+student("Aditya", int(input("Enter age of Aditya Raj:")))
+student("Rahul", int(input("Enter age of Rahul Kumar:")))
