06763b524bd779d50accddf4b6bfccf58dfae8b0
Aditya Raj <adityarajadityaraj893@gmail.com>
2026-07-11 17:11:04 +0530

    Create 06 square_of_number_using_function.py

---
+++ 12 Functions/06 square_of_number_using_function.py
@@ -0,0 +1,6 @@
+#Write a program to input number from user and print square of number using function in python.
+def sqr(a):
+    return a*a
+n=int(input("Enter a number:"))
+s=sqr(n)
+print("Square of number=",s)
