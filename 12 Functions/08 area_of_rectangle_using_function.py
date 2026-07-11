61826b1de46bc68b89a9b54eee9d08b66c96075c
Aditya Raj <adityarajadityaraj893@gmail.com>
2026-07-11 17:22:34 +0530

    Create 08 area_of_rectangle_using_function.py

---
+++ 12 Functions/08 area_of_rectangle_using_function.py
@@ -0,0 +1,7 @@
+#Write a function to find the area of a rectangle.
+def area(l,b):
+    return l*b
+l=int(input("Enter a number:"))
+b=int(input("Enter a number:"))
+s=area(l,b)
+print("area of rectangle=",s)
