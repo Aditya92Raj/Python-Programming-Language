70000de356edcf9ad666de87e074c564cb591ce9
Aditya Raj <adityarajadityaraj893@gmail.com>
2026-07-11 14:39:42 +0530

    Create 04 Addition_program_using_function.py

---
+++ 12 Functions/04 Addition_program_using_function.py
@@ -0,0 +1,14 @@
+# Addition Program using Function
+
+# Function Definition
+def add(a, b):
+    return a + b
+
+# User Input
+num1 = int(input("Enter first number:"))
+num2 = int(input("Enter second number:"))
+
+# Function Call
+result = add(num1, num2)
+
+print("Addition =", result)
