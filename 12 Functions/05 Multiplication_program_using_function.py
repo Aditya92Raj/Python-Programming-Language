1611dcda85a6bf605b1baea6c3c63fe339664d18
Aditya Raj <adityarajadityaraj893@gmail.com>
2026-07-11 14:48:29 +0530

    Create 05 Multiplication_program_using_function.py

---
+++ 12 Functions/05 Multiplication_program_using_function.py
@@ -0,0 +1,14 @@
+# Multiplication Program using Function
+
+# Function Definition
+def product(a, b):
+    return a * b
+
+# User Input
+num1 = int(input("Enter first number:"))
+num2 = int(input("Enter second number:"))
+
+# Function Call
+result = product(num1, num2)
+
+print("product =", result)
