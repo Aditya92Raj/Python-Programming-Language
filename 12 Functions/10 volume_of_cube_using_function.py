2f1dfa67386a9b801a5a06351b6dd0a637b5205e
Aditya Raj <adityarajadityaraj893@gmail.com>
2026-07-12 18:47:49 +0530

    Update 10 volume_of_cube_using_function.py

--- 12 Functions/10 volume_of_cube_using_function.py
+++ 12 Functions/10 volume_of_cube_using_function.py
@@ -1,6 +1,6 @@
 #Write a function to find the volume of a cube.
 def volume(a):
     return a*a*a
-a=int(input("Enter a number:"))
+a=int(input("Enter a side:"))
 s=volume(a)
 print("volume of cube=",s)
