e795042b2671e1250217e50925b907849790e9f2
Aditya Raj <adityarajadityaraj893@gmail.com>
2026-07-15 23:01:01 +0530

    Create 03 get()_method.py

---
+++ 08 Dictionaries/03 Dictionary Method/03 get()_method.py
@@ -0,0 +1,8 @@
+#Dictionary method in python:-
+#get() method is used to access value of keys from dictionary.
+student={
+    "name":"Aditya Raj",
+    "age":20,
+    "course":"BCA"
+}
+print(student.get("name"))
