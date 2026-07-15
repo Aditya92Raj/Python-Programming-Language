2b9c275a0c8806b2ae2c8eab5f9d20c40a7cd4a4
Aditya Raj <adityarajadityaraj893@gmail.com>
2026-07-15 22:44:32 +0530

    Create 01 clear()_method.py

---
+++ 08 Dictionaries/03 Dictionary Method/01 clear()_method.py
@@ -0,0 +1,13 @@
+#Dictionary method in python:-
+#clear() method is used to delete all key-value pairs.
+student={
+    "name":"Aditya",
+    "age":20,
+    "course":"BCA"
+}
+print("Before used clear()")
+print("student=",student)
+student.clear()
+print("After used clear()")
+print("student=",student)
+#after used clear method dictionary will empty.
