e066740abf4087296f3bef25d641c23bf6afc37d
Aditya Raj <adityarajadityaraj893@gmail.com>
2026-07-15 23:04:27 +0530

    Create 04 items_method.py

---
+++ 08 Dictionaries/03 Dictionary Method/04 items_method.py
@@ -0,0 +1,8 @@
+#Dictionary method in python:-
+#items method is used to assess key-value in tuple form.
+student={
+    "name":"Aditya",
+    "age":20,
+    "course":"BCA"
+}
+print(student.items())
