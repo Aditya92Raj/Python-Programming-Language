788303941e61ce22b048e1dc56c04074812c6e1d
Aditya Raj <adityarajadityaraj893@gmail.com>
2026-07-15 22:50:30 +0530

    Create 02 copy()_method.py

---
+++ 08 Dictionaries/03 Dictionary Method/02 copy()_method.py
@@ -0,0 +1,10 @@
+#Dictionary method in python:-
+#copy() method is used to copy one dictionary key-value into another dictionary 
+student = {
+    "name":"Aditya",
+    "age":20,
+    "course":"BCA"
+}
+print("Original Dictionary=",student)
+student_copy=student.copy()
+print("Copied Dictionary=",student_copy)
