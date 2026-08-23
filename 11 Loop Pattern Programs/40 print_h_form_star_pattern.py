5fc3851c004001194ca40a27967bf565ba8acebe
Aditya Raj <adityarajadityaraj893@gmail.com>
2026-08-23 23:23:22 +0530

    Create 40 print_h_form_star_pattern.py

---
+++ 11 Loop Pattern Programs/40 print_h_form_star_pattern.py
@@ -0,0 +1,9 @@
+#Write a program to print h form star pattern.
+a=int(input("Enter a number:"))
+for i in range(1,a+1):
+    for j in range(1,a+1):
+        if(j==1 or (i==a//2+1 and j!=a) or (j==a and i>a//2+1)):
+            print("*",end="")
+        else:
+            print(" ", end="")
+    print()
