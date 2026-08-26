2aead012b8ca7834984b2436f4bed3b241f6a1b1
Aditya Raj <adityarajadityaraj893@gmail.com>
2026-08-26 23:08:38 +0530

    Create 43 print_*_form_star_pattern.py

---
+++ 11 Loop Pattern Programs/43 print_*_form_star_pattern.py
@@ -0,0 +1,9 @@
+#Write a program to print * form star pattern.
+a=int(input("Enter a number:"))
+for i in range(1,a+1):
+    for j in range(1,a+1):
+        if (i==a//2+1 or j==a//2+1 or i==j or j==a+1-i):
+            print("*",end="")
+        else:
+            print(" ", end="")
+    print()
